2cbf282aa515b994ab9b10ea9d6379fd1db89a75
Joe Howarth <josephehowarth@gmail.com>
2023-06-13 09:51:42 -0700

    basics

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,69 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/WormholeRelayerSDK.sol";
+import "../src/interfaces/IWormholeReceiver.sol";
+import "../src/interfaces/IWormholeRelayer.sol";
+import "../src/interfaces/IERC20.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+import "../src/testing/WormholeRelayerTest.sol";
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+import "forge-std/console.sol";
+
+contract Toy is IWormholeReceiver {
+    IWormholeRelayer relayer;
+
+    uint public payloadReceived;
+
+    constructor(address _wormholeRelayer) {
+        relayer = IWormholeRelayer(_wormholeRelayer);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function receiveWormholeMessages(
+        bytes memory payload,
+        bytes[] memory additionalVaas,
+        bytes32, //sourceAddress,
+        uint16, //sourceChain,
+        bytes32 //deliveryHash
+    ) public payable {
+        require(msg.sender == address(relayer), "Only relayer can call");
+        payloadReceived = abi.decode(payload, (uint));
+
+        console.log("Toy received message");
+        console.log("Payload", payloadReceived);
+        console.log("Num additional vaas", additionalVaas.length);
+    }
+}
+
+contract WormholeSDKTest is WormholeRelayerTest {
+    IERC20 token;
+    Toy toy;
+
+    function setUpSource() public override {
+        token = createAndAttestToken(sourceFork);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function setUpTarget() public override {
+        toy = new Toy(address(relayerTarget));
+    }
+
+    function testSendToken() public {
+        vm.recordLogs();
+        (uint cost, ) = relayerSource.quoteEVMDeliveryPrice(
+            targetChain,
+            1e17,
+            50_000
+        );
+        relayerSource.sendPayloadToEvm{value: cost}(
+            targetChain,
+            address(toy),
+            abi.encode(55),
+            1e17,
+            50_000
+        );
+        performDelivery(1);
+
+        vm.selectFork(targetFork);
+        require(55 == toy.payloadReceived());
     }
 }
