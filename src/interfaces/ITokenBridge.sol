3a68955367f94245872056ccb4d8333c04c8c863
nonergodic <nonergodic@gmail.com>
2024-09-18 22:11:33 -0700

    add token bridge TransferRedeemed event and unify formatting

--- src/interfaces/ITokenBridge.sol
+++ src/interfaces/ITokenBridge.sol
@@ -6,172 +6,189 @@ pragma solidity ^0.8.0;
 import "./IWormhole.sol";
 
 interface ITokenBridge {
-    struct Transfer {
-        uint8 payloadID;
-        uint256 amount;
-        bytes32 tokenAddress;
-        uint16 tokenChain;
-        bytes32 to;
-        uint16 toChain;
-        uint256 fee;
-    }
+  struct Transfer {
+    uint8 payloadID;
+    uint256 amount;
+    bytes32 tokenAddress;
+    uint16 tokenChain;
+    bytes32 to;
+    uint16 toChain;
+    uint256 fee;
+  }
 
-    struct TransferWithPayload {
-        uint8 payloadID;
-        uint256 amount;
-        bytes32 tokenAddress;
-        uint16 tokenChain;
-        bytes32 to;
-        uint16 toChain;
-        bytes32 fromAddress;
-        bytes payload;
-    }
+  struct TransferWithPayload {
+    uint8 payloadID;
+    uint256 amount;
+    bytes32 tokenAddress;
+    uint16 tokenChain;
+    bytes32 to;
+    uint16 toChain;
+    bytes32 fromAddress;
+    bytes payload;
+  }
 
-    struct AssetMeta {
-        uint8 payloadID;
-        bytes32 tokenAddress;
-        uint16 tokenChain;
-        uint8 decimals;
-        bytes32 symbol;
-        bytes32 name;
-    }
+  struct AssetMeta {
+    uint8 payloadID;
+    bytes32 tokenAddress;
+    uint16 tokenChain;
+    uint8 decimals;
+    bytes32 symbol;
+    bytes32 name;
+  }
 
-    struct RegisterChain {
-        bytes32 module;
-        uint8 action;
-        uint16 chainId;
-        uint16 emitterChainID;
-        bytes32 emitterAddress;
-    }
+  struct RegisterChain {
+    bytes32 module;
+    uint8 action;
+    uint16 chainId;
+    uint16 emitterChainID;
+    bytes32 emitterAddress;
+  }
 
-    struct UpgradeContract {
-        bytes32 module;
-        uint8 action;
-        uint16 chainId;
-        bytes32 newContract;
-    }
+  struct UpgradeContract {
+    bytes32 module;
+    uint8 action;
+    uint16 chainId;
+    bytes32 newContract;
+  }
 
-    struct RecoverChainId {
-        bytes32 module;
-        uint8 action;
-        uint256 evmChainId;
-        uint16 newChainId;
-    }
+  struct RecoverChainId {
+    bytes32 module;
+    uint8 action;
+    uint256 evmChainId;
+    uint16 newChainId;
+  }
 
-    event ContractUpgraded(address indexed oldContract, address indexed newContract);
+  event ContractUpgraded(address indexed oldContract, address indexed newContract);
 
-    function _parseTransferCommon(bytes memory encoded) external pure returns (Transfer memory transfer);
+  event TransferRedeemed(
+    uint16 indexed emitterChainId,
+    bytes32 indexed emitterAddress,
+    uint64 indexed sequence
+  );
 
-    function attestToken(address tokenAddress, uint32 nonce) external payable returns (uint64 sequence);
+  function _parseTransferCommon(
+    bytes memory encoded
+  ) external pure returns (Transfer memory transfer);
 
-    function wrapAndTransferETH(uint16 recipientChain, bytes32 recipient, uint256 arbiterFee, uint32 nonce)
-        external
-        payable
-        returns (uint64 sequence);
+  function attestToken(
+    address tokenAddress,
+    uint32 nonce
+  ) external payable returns (uint64 sequence);
 
-    function wrapAndTransferETHWithPayload(uint16 recipientChain, bytes32 recipient, uint32 nonce, bytes memory payload)
-        external
-        payable
-        returns (uint64 sequence);
+  function wrapAndTransferETH(
+    uint16 recipientChain,
+    bytes32 recipient,
+    uint256 arbiterFee,
+    uint32 nonce
+  ) external payable returns (uint64 sequence);
 
-    function transferTokens(
-        address token,
-        uint256 amount,
-        uint16 recipientChain,
-        bytes32 recipient,
-        uint256 arbiterFee,
-        uint32 nonce
-    ) external payable returns (uint64 sequence);
+  function wrapAndTransferETHWithPayload(
+    uint16 recipientChain,
+    bytes32 recipient,
+    uint32 nonce,
+    bytes memory payload
+  ) external payable returns (uint64 sequence);
 
-    function transferTokensWithPayload(
-        address token,
-        uint256 amount,
-        uint16 recipientChain,
-        bytes32 recipient,
-        uint32 nonce,
-        bytes memory payload
-    ) external payable returns (uint64 sequence);
+  function transferTokens(
+    address token,
+    uint256 amount,
+    uint16 recipientChain,
+    bytes32 recipient,
+    uint256 arbiterFee,
+    uint32 nonce
+  ) external payable returns (uint64 sequence);
 
-    function updateWrapped(bytes memory encodedVm) external returns (address token);
+  function transferTokensWithPayload(
+    address token,
+    uint256 amount,
+    uint16 recipientChain,
+    bytes32 recipient,
+    uint32 nonce,
+    bytes memory payload
+  ) external payable returns (uint64 sequence);
 
-    function createWrapped(bytes memory encodedVm) external returns (address token);
+  function updateWrapped(bytes memory encodedVm) external returns (address token);
 
-    function completeTransferWithPayload(bytes memory encodedVm) external returns (bytes memory);
+  function createWrapped(bytes memory encodedVm) external returns (address token);
 
-    function completeTransferAndUnwrapETHWithPayload(bytes memory encodedVm) external returns (bytes memory);
+  function completeTransferWithPayload(bytes memory encodedVm) external returns (bytes memory);
 
-    function completeTransfer(bytes memory encodedVm) external;
+  function completeTransferAndUnwrapETHWithPayload(
+    bytes memory encodedVm
+  ) external returns (bytes memory);
 
-    function completeTransferAndUnwrapETH(bytes memory encodedVm) external;
+  function completeTransfer(bytes memory encodedVm) external;
 
-    function encodeAssetMeta(AssetMeta memory meta) external pure returns (bytes memory encoded);
+  function completeTransferAndUnwrapETH(bytes memory encodedVm) external;
 
-    function encodeTransfer(Transfer memory transfer) external pure returns (bytes memory encoded);
+  function encodeAssetMeta(AssetMeta memory meta) external pure returns (bytes memory encoded);
 
-    function encodeTransferWithPayload(TransferWithPayload memory transfer)
-        external
-        pure
-        returns (bytes memory encoded);
+  function encodeTransfer(Transfer memory transfer) external pure returns (bytes memory encoded);
 
-    function parsePayloadID(bytes memory encoded) external pure returns (uint8 payloadID);
+  function encodeTransferWithPayload(TransferWithPayload memory transfer)
+    external
+    pure
+    returns (bytes memory encoded);
 
-    function parseAssetMeta(bytes memory encoded) external pure returns (AssetMeta memory meta);
+  function parsePayloadID(bytes memory encoded) external pure returns (uint8 payloadID);
 
-    function parseTransfer(bytes memory encoded) external pure returns (Transfer memory transfer);
+  function parseAssetMeta(bytes memory encoded) external pure returns (AssetMeta memory meta);
 
-    function parseTransferWithPayload(bytes memory encoded)
-        external
-        pure
-        returns (TransferWithPayload memory transfer);
+  function parseTransfer(bytes memory encoded) external pure returns (Transfer memory transfer);
 
-    function governanceActionIsConsumed(bytes32 hash) external view returns (bool);
+  function parseTransferWithPayload(bytes memory encoded)
+    external
+    pure
+    returns (TransferWithPayload memory transfer);
 
-    function isInitialized(address impl) external view returns (bool);
+  function governanceActionIsConsumed(bytes32 hash) external view returns (bool);
 
-    function isTransferCompleted(bytes32 hash) external view returns (bool);
+  function isInitialized(address impl) external view returns (bool);
 
-    function wormhole() external view returns (IWormhole);
+  function isTransferCompleted(bytes32 hash) external view returns (bool);
 
-    function chainId() external view returns (uint16);
+  function wormhole() external view returns (IWormhole);
 
-    function evmChainId() external view returns (uint256);
+  function chainId() external view returns (uint16);
 
-    function isFork() external view returns (bool);
+  function evmChainId() external view returns (uint256);
 
-    function governanceChainId() external view returns (uint16);
+  function isFork() external view returns (bool);
 
-    function governanceContract() external view returns (bytes32);
+  function governanceChainId() external view returns (uint16);
 
-    function wrappedAsset(uint16 tokenChainId, bytes32 tokenAddress) external view returns (address);
+  function governanceContract() external view returns (bytes32);
 
-    function bridgeContracts(uint16 chainId_) external view returns (bytes32);
+  function wrappedAsset(uint16 tokenChainId, bytes32 tokenAddress) external view returns (address);
 
-    function tokenImplementation() external view returns (address);
+  function bridgeContracts(uint16 chainId_) external view returns (bytes32);
 
-    function WETH() external view returns (address);
+  function tokenImplementation() external view returns (address);
 
-    function outstandingBridged(address token) external view returns (uint256);
+  function WETH() external view returns (address);
 
-    function isWrappedAsset(address token) external view returns (bool);
+  function outstandingBridged(address token) external view returns (uint256);
 
-    function finality() external view returns (uint8);
+  function isWrappedAsset(address token) external view returns (bool);
 
-    function implementation() external view returns (address);
+  function finality() external view returns (uint8);
 
-    function initialize() external;
+  function implementation() external view returns (address);
 
-    function registerChain(bytes memory encodedVM) external;
+  function initialize() external;
 
-    function upgrade(bytes memory encodedVM) external;
+  function registerChain(bytes memory encodedVM) external;
 
-    function submitRecoverChainId(bytes memory encodedVM) external;
+  function upgrade(bytes memory encodedVM) external;
 
-    function parseRegisterChain(bytes memory encoded) external pure returns (RegisterChain memory chain);
+  function submitRecoverChainId(bytes memory encodedVM) external;
 
-    function parseUpgrade(bytes memory encoded) external pure returns (UpgradeContract memory chain);
+  function parseRegisterChain(bytes memory encoded) external pure returns (RegisterChain memory chain);
 
-    function parseRecoverChainId(bytes memory encodedRecoverChainId)
-        external
-        pure
-        returns (RecoverChainId memory rci);
+  function parseUpgrade(bytes memory encoded) external pure returns (UpgradeContract memory chain);
+
+  function parseRecoverChainId(bytes memory encodedRecoverChainId)
+    external
+    pure
+    returns (RecoverChainId memory rci);
 }
