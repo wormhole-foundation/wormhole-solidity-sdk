eff3062985804d4b2b3496e3408e3385d528dee9
Andreas <41449730+nonergodic@users.noreply.github.com>
2024-09-24 10:37:28 -0700

    add token bridge TransferRedeemed event and unify formatting (#61)
    * add token bridge TransferRedeemed event and unify formatting
    
    * more formatting

--- src/interfaces/IWormhole.sol
+++ src/interfaces/IWormhole.sol
@@ -1,148 +1,160 @@
-// contracts/Messages.sol
 // SPDX-License-Identifier: Apache 2
 
 pragma solidity ^0.8.0;
 
 interface IWormhole {
-    struct GuardianSet {
-        address[] keys;
-        uint32 expirationTime;
-    }
+  struct GuardianSet {
+    address[] keys;
+    uint32 expirationTime;
+  }
 
-    struct Signature {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-        uint8 guardianIndex;
-    }
+  struct Signature {
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    uint8 guardianIndex;
+  }
 
-    struct VM {
-        uint8 version;
-        uint32 timestamp;
-        uint32 nonce;
-        uint16 emitterChainId;
-        bytes32 emitterAddress;
-        uint64 sequence;
-        uint8 consistencyLevel;
-        bytes payload;
-        uint32 guardianSetIndex;
-        Signature[] signatures;
-        bytes32 hash;
-    }
+  struct VM {
+    uint8 version;
+    uint32 timestamp;
+    uint32 nonce;
+    uint16 emitterChainId;
+    bytes32 emitterAddress;
+    uint64 sequence;
+    uint8 consistencyLevel;
+    bytes payload;
+    uint32 guardianSetIndex;
+    Signature[] signatures;
+    bytes32 hash;
+  }
 
-    struct ContractUpgrade {
-        bytes32 module;
-        uint8 action;
-        uint16 chain;
-        address newContract;
-    }
+  struct ContractUpgrade {
+    bytes32 module;
+    uint8 action;
+    uint16 chain;
+    address newContract;
+  }
 
-    struct GuardianSetUpgrade {
-        bytes32 module;
-        uint8 action;
-        uint16 chain;
-        GuardianSet newGuardianSet;
-        uint32 newGuardianSetIndex;
-    }
+  struct GuardianSetUpgrade {
+    bytes32 module;
+    uint8 action;
+    uint16 chain;
+    GuardianSet newGuardianSet;
+    uint32 newGuardianSetIndex;
+  }
 
-    struct SetMessageFee {
-        bytes32 module;
-        uint8 action;
-        uint16 chain;
-        uint256 messageFee;
-    }
+  struct SetMessageFee {
+    bytes32 module;
+    uint8 action;
+    uint16 chain;
+    uint256 messageFee;
+  }
 
-    struct TransferFees {
-        bytes32 module;
-        uint8 action;
-        uint16 chain;
-        uint256 amount;
-        bytes32 recipient;
-    }
+  struct TransferFees {
+    bytes32 module;
+    uint8 action;
+    uint16 chain;
+    uint256 amount;
+    bytes32 recipient;
+  }
 
-    struct RecoverChainId {
-        bytes32 module;
-        uint8 action;
-        uint256 evmChainId;
-        uint16 newChainId;
-    }
+  struct RecoverChainId {
+    bytes32 module;
+    uint8 action;
+    uint256 evmChainId;
+    uint16 newChainId;
+  }
 
-    event LogMessagePublished(
-        address indexed sender, uint64 sequence, uint32 nonce, bytes payload, uint8 consistencyLevel
-    );
-    event ContractUpgraded(address indexed oldContract, address indexed newContract);
-    event GuardianSetAdded(uint32 indexed index);
+  event LogMessagePublished(
+    address indexed sender,
+    uint64 sequence,
+    uint32 nonce,
+    bytes payload,
+    uint8 consistencyLevel
+  );
 
-    function publishMessage(uint32 nonce, bytes memory payload, uint8 consistencyLevel)
-        external
-        payable
-        returns (uint64 sequence);
+  event ContractUpgraded(address indexed oldContract, address indexed newContract);
 
-    function initialize() external;
+  event GuardianSetAdded(uint32 indexed index);
 
-    function parseAndVerifyVM(bytes calldata encodedVM)
-        external
-        view
-        returns (VM memory vm, bool valid, string memory reason);
+  function publishMessage(uint32 nonce, bytes memory payload, uint8 consistencyLevel)
+    external
+    payable
+    returns (uint64 sequence);
 
-    function verifyVM(VM memory vm) external view returns (bool valid, string memory reason);
+  function initialize() external;
 
-    function verifySignatures(bytes32 hash, Signature[] memory signatures, GuardianSet memory guardianSet)
-        external
-        pure
-        returns (bool valid, string memory reason);
+  function parseAndVerifyVM(bytes calldata encodedVM)
+    external
+    view
+    returns (VM memory vm, bool valid, string memory reason);
 
-    function parseVM(bytes memory encodedVM) external pure returns (VM memory vm);
+  function verifyVM(VM memory vm) external view returns (bool valid, string memory reason);
 
-    function quorum(uint256 numGuardians) external pure returns (uint256 numSignaturesRequiredForQuorum);
+  function verifySignatures(
+    bytes32 hash,
+    Signature[] memory signatures,
+    GuardianSet memory guardianSet
+  ) external pure returns (bool valid, string memory reason);
 
-    function getGuardianSet(uint32 index) external view returns (GuardianSet memory);
+  function parseVM(bytes memory encodedVM) external pure returns (VM memory vm);
 
-    function getCurrentGuardianSetIndex() external view returns (uint32);
+  function quorum(
+    uint256 numGuardians
+  ) external pure returns (uint256 numSignaturesRequiredForQuorum);
 
-    function getGuardianSetExpiry() external view returns (uint32);
+  function getGuardianSet(uint32 index) external view returns (GuardianSet memory);
 
-    function governanceActionIsConsumed(bytes32 hash) external view returns (bool);
+  function getCurrentGuardianSetIndex() external view returns (uint32);
 
-    function isInitialized(address impl) external view returns (bool);
+  function getGuardianSetExpiry() external view returns (uint32);
 
-    function chainId() external view returns (uint16);
+  function governanceActionIsConsumed(bytes32 hash) external view returns (bool);
 
-    function isFork() external view returns (bool);
+  function isInitialized(address impl) external view returns (bool);
 
-    function governanceChainId() external view returns (uint16);
+  function chainId() external view returns (uint16);
 
-    function governanceContract() external view returns (bytes32);
+  function isFork() external view returns (bool);
 
-    function messageFee() external view returns (uint256);
+  function governanceChainId() external view returns (uint16);
 
-    function evmChainId() external view returns (uint256);
+  function governanceContract() external view returns (bytes32);
 
-    function nextSequence(address emitter) external view returns (uint64);
+  function messageFee() external view returns (uint256);
 
-    function parseContractUpgrade(bytes memory encodedUpgrade) external pure returns (ContractUpgrade memory cu);
+  function evmChainId() external view returns (uint256);
 
-    function parseGuardianSetUpgrade(bytes memory encodedUpgrade)
-        external
-        pure
-        returns (GuardianSetUpgrade memory gsu);
+  function nextSequence(address emitter) external view returns (uint64);
 
-    function parseSetMessageFee(bytes memory encodedSetMessageFee) external pure returns (SetMessageFee memory smf);
+  function parseContractUpgrade(
+    bytes memory encodedUpgrade
+  ) external pure returns (ContractUpgrade memory cu);
 
-    function parseTransferFees(bytes memory encodedTransferFees) external pure returns (TransferFees memory tf);
+  function parseGuardianSetUpgrade(
+    bytes memory encodedUpgrade
+  ) external pure returns (GuardianSetUpgrade memory gsu);
 
-    function parseRecoverChainId(bytes memory encodedRecoverChainId)
-        external
-        pure
-        returns (RecoverChainId memory rci);
+  function parseSetMessageFee(
+    bytes memory encodedSetMessageFee
+  ) external pure returns (SetMessageFee memory smf);
 
-    function submitContractUpgrade(bytes memory _vm) external;
+  function parseTransferFees(
+    bytes memory encodedTransferFees
+  ) external pure returns (TransferFees memory tf);
 
-    function submitSetMessageFee(bytes memory _vm) external;
+  function parseRecoverChainId(
+    bytes memory encodedRecoverChainId
+  ) external pure returns (RecoverChainId memory rci);
 
-    function submitNewGuardianSet(bytes memory _vm) external;
+  function submitContractUpgrade(bytes memory _vm) external;
 
-    function submitTransferFees(bytes memory _vm) external;
+  function submitSetMessageFee(bytes memory _vm) external;
 
-    function submitRecoverChainId(bytes memory _vm) external;
+  function submitNewGuardianSet(bytes memory _vm) external;
+
+  function submitTransferFees(bytes memory _vm) external;
+
+  function submitRecoverChainId(bytes memory _vm) external;
 }
